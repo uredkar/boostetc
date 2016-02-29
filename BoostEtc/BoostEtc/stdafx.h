bcdd77dd8bf9e68a9b8335442485581548db330f
uredkar <unmesh_redkar@hotmail.com>
2016-02-29 16:17:32 -0600

    adding boost code

--- BoostEtc/BoostEtc/stdafx.h
+++ BoostEtc/BoostEtc/stdafx.h
@@ -11,5 +11,25 @@
 #include <tchar.h>
 
 
+#define BOOST_TEST_NO_MAIN
+#define BOOST_TEST_MODULE tolerance_01
+
+
+
+
+#include <iomanip> // for std::setprecision()
+#include <iostream>
+
+
+#include<boost/range/numeric.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/moment.hpp>
+#include <boost/math/constants/constants.hpp>
+
+using namespace boost::accumulators;
+using namespace boost::numeric;
+using namespace std;
 
 // TODO: reference additional headers your program requires here
